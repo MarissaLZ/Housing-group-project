cf9ef5d9beee1b713364cb2b7a36d67d676280fc
Joe <joe@csgofoodreviews.com>
2021-06-24 19:22:39 -0400

    Created a main java class

---
+++ src/charlieteam/HouseApp.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package charlieteam;
+
+/**
+ * @author Joe, Marissa, Kathleen, & Peter
+ *
+ */
+public class HouseApp
+{
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+}
