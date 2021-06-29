c826267a40f9a2e752c93e3b200ab69475ead1c1
Joe <joe@csgofoodreviews.com>
2021-06-29 11:39:07 -0400

    Prep Subdivison class

--- src/charlieteam/SubDivision.java
+++ src/charlieteam/SubDivision.java
@@ -4,19 +4,10 @@
 package charlieteam;
 
 /**
- * @author Joe
+ * @author Charlie Team
  *
  */
 public class SubDivision
 {
-
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args)
-	{
-		// TODO Auto-generated method stub
-
-	}
 
 }
