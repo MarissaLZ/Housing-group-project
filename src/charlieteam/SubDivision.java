26a5c89780aa0edd1689c021f26da15cbf9630df
Mlazo4 <Mlazo4@csgofoodreviews.com>
2021-07-01 20:07:21 -0400

    committing because of refactor error

--- src/charlieteam/SubDivision.java
+++ src/charlieteam/SubDivision.java
@@ -4,10 +4,47 @@
 package charlieteam;
 
 /**
- * @author Charlie Team
+ * @author Joe
  *
  */
-public class SubDivision
-{
+public class SubDivision {
+	/**
+	 * @param args
+	 */
+	 
+	 //instance variable
+	 //House specifies the type therefore input must be of the class House type
+	 private ArrayList<House> houses = new ArrayList<>();
 
+	 
+	 //default constructor and we know object is created
+	 //should I declare as public?
+	 public subDivision () {
+	 }
+	 
+	 
+	 //methods
+	 //adds a house
+	public boolean add(House h) {
+	    houses.add(h);
+	    
+	    if (houses.contains(h)) {
+	        return true;
+	    } 
+	    else {
+	        return false;
+	    }
+	    
+        //accessor method to get house object
+        public House get(int i) {
+            return houses.get(i);
+            
+        }
+        
+	    
+	    
+	    
+	     
+	 }
+	 
 }
