2ab1c35673f1235cb671d58892d90f717e40c1bd
Joe <joe@csgofoodreviews.com>
2021-07-04 16:15:50 -0400

    Clean up Subdivision.java

--- src/charlieteam/Subdivision.java
+++ src/charlieteam/Subdivision.java
@@ -2,58 +2,70 @@
  * 
  */
 package charlieteam;
-import java.util.*;
+
+import java.util.ArrayList;
+
 /**
- * @author Joe
+ * @author Charlie Team
  *
  */
-public class Subdivision {
-	/**
-	 * @param args
-	 */
-	
+public class Subdivision
+{
+
 	// instance variable
 	private ArrayList<House> houses = new ArrayList<>();
 
 	// default constructor
-	public Subdivision () {
+	public Subdivision()
+	{
 	}
 
-	//Adds a house to the subdivision
-	public boolean add(House h) {
+	// Adds a house to the subdivision
+	public boolean add(House h)
+	{
 		houses.add(h);
-	    if (houses.contains(h)) {
-	        return true;
-	    } 
-	    else {
-	        return false;
-	    }
-	}  
-	
-	//Accessor method to get a house object
-	public House get(int i) {
+		if (houses.contains(h))
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+
+	// Accessor method to get a house object
+	public House get(int i)
+	{
 		return houses.get(i);
 	}
-	
-	public int size() {
-		return houses.size();	
+
+	public int size()
+	{
+		return houses.size();
 	}
-	
-	//Returns list of houses
-	public ArrayList<House> list() {
+
+	// Returns list of houses
+	public ArrayList<House> list()
+	{
 		return houses;
 	}
-    
-	//overrides toString() and returns a string of house objects in the subdivision
-	public String toString() {
+
+	// overrides toString() and returns a string of house objects in the
+	// subdivision
+	public String toString()
+	{
 		String str = "";
-		for (House element: houses) {
-			 str+= element+"\n\n";
+		for (House element : houses)
+		{
+			str += element + "\n\n";
 		}
-		return str;	 
+		return str;
 	}
+
 	// returns true or false if a given String characteristic is found
-	public boolean find(String characteristic) {
+	public boolean find(String characteristic)
+	{
 		return this.toString().contains(characteristic);
-	}	      
+	}
 }
