105f21686ad91632451014619dc7c4b02838eccf
Peter <pvscott16@csgofoodreviews.com>
2021-07-06 22:56:52 -0400

    Add the four methods to Subdivision and add some testing code to SubdivisionTester and commented out not necessary test the new methods

--- src/charlieteam/Subdivision.java
+++ src/charlieteam/Subdivision.java
@@ -70,6 +70,71 @@ public class Subdivision
 	}
 
 	// Step 3.
-
+	// returns an array of all the houses that the total area are equal to or between the given floor and ceiling 
+	public ArrayList<House> listByArea(double floor, double ceiling){
+		ArrayList<House> areaList = new ArrayList<>();
+		for (int i = 0; i < houses.size(); i++) {
+			if (houses.get(i).getTotalArea() >= floor && houses.get(i).getTotalArea() <= ceiling ){
+				areaList.add(houses.get(i));
+			}
+		}
+		return areaList;	
+		
+	}
+	// returns an array of all the houses that the plot size are equal to or between the given floor and ceiling 
+	public ArrayList<House> listByPlot(double floor, double ceiling){
+		ArrayList<House> plotList = new ArrayList<>();
+		for (int i = 0; i < houses.size(); i++) {
+			if (houses.get(i).getPlot() >= floor && houses.get(i).getPlot() <= ceiling ){
+				plotList.add(houses.get(i));
+			}
+		}
+		return plotList;
+		
+	}
+	// returns a sorted array by the total area from smallest to largest
+	public ArrayList<House> sortByArea(){
+		ArrayList<House> areaSort = new ArrayList<>(houses);
+		for (int i = 0; i < areaSort.size(); i++) {
+			House hMinArea = areaSort.get(i);
+			int minIndex = i;
+			for (int j = i + 1; j < areaSort.size(); j++) {
+				 if (hMinArea.getTotalArea() > areaSort.get(j).getTotalArea()) {
+					 hMinArea = areaSort.get(j);
+					 minIndex = j;
+				 }
+			}
+			if (minIndex != i) {
+				areaSort.set(minIndex, areaSort.get(i));
+				areaSort.set(i, hMinArea);
+				
+			}
+			
+		}
+ 		return areaSort;
+		
+	}
+	// returns a sorted array by the plot size from smallest to largest
+	public ArrayList<House> sortByPlot(){
+		ArrayList<House> plotSort = new ArrayList<>(houses);
+		for (int i = 0; i < plotSort.size(); i++) {
+			House hMinArea = plotSort.get(i);
+			int minIndex = i;
+			for (int j = i + 1; j < plotSort.size(); j++) {
+				 if (hMinArea.getPlot() > plotSort.get(j).getPlot()) {
+					 hMinArea = plotSort.get(j);
+					 minIndex = j;
+				 }
+			}
+			if (minIndex != i) {
+				plotSort.set(minIndex, plotSort.get(i));
+				plotSort.set(i, hMinArea);
+				
+			}
+			
+		}
+ 		return plotSort;
+		
+	}
 	// Step 4.
 }
--- src/charlieteam/SubdivisionTester.java
+++ src/charlieteam/SubdivisionTester.java
@@ -13,18 +13,43 @@ public class SubdivisionTester {
 		h1.setPlot(1.0);		
 		
 		House h2 = new House("Colonial", 200, 300, 1, 0.5);
+		House h3 = new House("Victorain", 500, 700, 4, 1.5);
+		House h4 = new House("Moblie", 100, 150, 1, 0.5);
+		House h5 = new House("Colonial", 300, 400, 2, 1.0);
 
 		Subdivision subdivision = new Subdivision();
 		subdivision.add(h1);
 		subdivision.add(h2);
+		subdivision.add(h3);
+		subdivision.add(h4);
+		subdivision.add(h5);
 		
-		System.out.println(subdivision.list());
-		System.out.println(subdivision.size());
+		//System.out.println(subdivision.list());
+		//System.out.println(subdivision.size());
 		
-		System.out.println(subdivision.find("Ranch"));
-		System.out.println(subdivision.get(1));
+		//System.out.println(subdivision.find("Ranch"));
+		//System.out.println(subdivision.get(1));
 		
+		//System.out.println(subdivision.toString());
+		
+		for(House element : subdivision.listByArea(750, 1000)) {
+			System.out.println(element);
+		}
+		System.out.println();
+		for(House element : subdivision.listByPlot(1, 2 )) {
+			System.out.println(element);
+		}
+		System.out.println();
+		for(House element : subdivision.sortByArea()) {
+			System.out.println(element);
+		}
+		System.out.println();
+		for(House element : subdivision.sortByPlot()) {
+			System.out.println(element);
+		}
+		System.out.println();
 		System.out.println(subdivision.toString());
+		
 	}
 
 }
